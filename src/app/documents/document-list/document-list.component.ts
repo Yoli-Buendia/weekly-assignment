40e8519fe99b2d6d982b6910ec8529a1d3b00c6b
Yoli-Buendia <bue17003@byui.edu>
2024-02-12 22:37:07 -0600

    update

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -8,20 +8,8 @@ import { DocumentsService } from '../documents.service';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
-//@Output() selectedDocumentEvent = new EventEmitter();
 
-  documents: Document[] =[
-    /*new Document('1', 'Document 1', 'This is my dummy document 1.', 'https://www.byui.com/1'),
-    new Document('2', 'Document 2', 'This is my dummy document 2.', 'https://www.byui.com/2'),
-    new Document('3', 'Document 3', 'This is my dummy document 3.', 'https://www.byui.com/3'),
-    new Document('4', 'Document 4', 'This is my dummy document 4.', 'https://www.byui.com/4')*/
-  ];
-
- // @Output() selectedDocumentEvent = new EventEmitter<Document>();
-
-  /*onSelectedDocument(document: Document){
-    this.selectedDocumentEvent.emit(document);
-  }*/
+  documents: Document[] =[];
 
   onSelected(document: Document){
     this.documentService.documentSelectedEvent.emit(document);
